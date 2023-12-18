925262442a8f6da7b4e3fefc05f1d994f02ba18d
dannyCSStudent <danv8605@gmail.com>
2023-12-18 07:37:26 -0600

    Connected to MongoDB and Deployed app

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import Image from "next/image"
+import Link from "next/link"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href='/'>
+          <Image 
+            src="/assets/images/logo.webp"
+            alt="logo"
+            width={45}
+            height={45}
+          />
+        </Link>
+
+        <p>2023 Gallerya. All Rights Reserved.</p>
+      </div>
+    </footer>
   )
 }
 
